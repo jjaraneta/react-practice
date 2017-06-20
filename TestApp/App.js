09a247af828bfbb989fd9bd0be58f0f4fac484cc
fdmadmin <jeiel.araneta@flightdigitalmedia.com>
2017-06-20 14:32:24 +0800

    initial commit

--- TestApp/App.js
+++ TestApp/App.js
@@ -5,7 +5,7 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>For Testing</Text>
+        <Text>For Testingr</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
       </View>
